2c68a3f55bf87a3c00598b9c5a8917a65485fb8b
MauroMolino <Mauro.Molino@edu-its.it>
2025-10-27 13:01:25 +0100

    rework favourite on recipe_page

--- let_me_cook/lib/Pages/recipe_journal.dart
+++ let_me_cook/lib/Pages/recipe_journal.dart
@@ -83,7 +83,8 @@ class _RecipeJournalState extends State<RecipeJournal> {
                           Navigator.push(
                             context,
                             MaterialPageRoute(
-                              builder: (context) => CategoryPage(category: category),
+                              builder: (context) =>
+                                  CategoryPage(category: category),
                             ),
                           );
                         },
@@ -121,6 +122,7 @@ class _RecipeJournalState extends State<RecipeJournal> {
           ],
         ),
       ),
+      bottomNavigationBar: const BottomBar(),
     );
   }
 }
